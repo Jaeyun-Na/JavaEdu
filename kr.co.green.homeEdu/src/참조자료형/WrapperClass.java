51dc383d92dd60ec132d68714bd4f5fd77a99ddc
Jaeyun <Jaeyun@DESKTOP-TK0JOHI>
2024-04-29 14:08:06 +0900

    240429

--- kr.co.green.homeEdu/src/참조자료형/WrapperClass.java
+++ kr.co.green.homeEdu/src/참조자료형/WrapperClass.java
@@ -2,4 +2,30 @@ package 참조자료형;
 
 public class WrapperClass {
 
+	int i = 5;
+	
+	Integer integer = new Integer(5);
+//	integer => 5
+	Integer integer1 = new Integer(5);
+//	 integer1 => 5
+	Integer integer = Integer.valueOf(5);
+//	integer => 5
+	Integer integer1 = Integer.valueOf(5);
+//	integer1 => 5
+	Integer integer = new Integer("5234");
+//	integer =>5234
+	
+	Integer i1 = new Integer(5);
+//	i1 => 5
+	Integer i2 = new Integer(5);
+//	i2 => 5
+	Integer i3 = Integer.valueOf(5)
+//	i3 => 5
+	Integer i4 = Integer.valueOf(5)
+//	i4 => 5	
+	
+	i1 == i2
+//	false	
+	i3 == i4
+//	true			
 }
