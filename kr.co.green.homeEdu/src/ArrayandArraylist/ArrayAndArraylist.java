d9da2cf93b38553b7ca006637d95ee6c5941b6db
Jaeyun <Jaeyun@DESKTOP-TK0JOHI>
2024-05-17 13:04:50 +0900

    240517

---
+++ kr.co.green.homeEdu/src/ArrayandArraylist/ArrayAndArraylist.java
@@ -0,0 +1,43 @@
+package ArrayandArraylist;
+
+public class ArrayAndArraylist {
+	public static void main(String[] args) {
+		
+//		Student student = new Student(name, list of Marks);
+//
+//		int number = student.getNumberOfMarks();
+//		int sum = student.getNumberOfMarks();
+//		int maximumMark = student.getNumberOfMarks();
+//		int minimumMark = student.getNumberOfMarks();
+//		BigDecimal average = student.getNumberOfMarks();
+//		student.addNewMark(35);
+//		student.removeMarkAtIndex(5);
+		
+		
+/*
+		jshell
+		
+		각각 mark의 값을 지정해주어 따로따로 저장
+
+		int mark;
+		mark1 = 100;
+		int  mark2 = 75
+		int  mark3 = 60
+		int sum = mark1+mark2+mark3;
+		int  mark4 = 56
+		
+		sum = mark1+mark2+mark3+mark4;
+		
+		배열 marks에 각각의 수를 저장
+		
+		int[] marks = {75, 60, 56}
+		-> int[3] {75, 60, 56}
+		int sum = 0
+		for(int mark:marks){
+			sum = sum + mark;
+		}
+		sum
+		-> 191
+*/		
+	}
+}
