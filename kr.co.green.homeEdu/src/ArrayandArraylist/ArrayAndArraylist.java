a11dfbd467a044bc955c83e8fb1c6c8f5297460b
Jaeyun <Jaeyun@DESKTOP-TK0JOHI>
2024-05-21 15:47:03 +0900

    240521

--- kr.co.green.homeEdu/src/ArrayandArraylist/ArrayAndArraylist.java
+++ kr.co.green.homeEdu/src/ArrayandArraylist/ArrayAndArraylist.java
@@ -38,6 +38,48 @@ public class ArrayAndArraylist {
 		}
 		sum
 		-> 191
+		
+		240521
+		-jshell-
+		int[배열 값의 갯수 및 위치] 이름 = {배열 값(들)/컨텐츠}
+		
+		int[] mark = {1,2,3}
+		->	int[3] {1,2,3}
+		int[] marks2 = {1,2,3,4,5}
+		->	int[5]{1,2,3,4,5}
+		int[]marks2 = {1}
+		->	int[1]{1}
+		int[] marks2 = {}
+		->	int[0] { }
+		
+		배열 선언 쉬운것
+		int[] mark = {1,2,3}
+		배열 선언 정석
+		int [] marks3 = new int[5];
+		-> int[5] {0,0,0,0,0}
+		
+		marks3[0] = 10
+		-> marks3의 0번 자리에 10의 값을 주겠다.
+		
+		marks3[0] = 1
+		marks3[1] = 2
+		marks3[2] = 3
+		marks3[3] = 4
+		marks3[4] = 5
+		
+		marks3
+		->	int[5]{1,2,3,4,5}
+		
+		marks2.length ->속성 메소드가 아님, 배열에서만 속성ㄹ으로 취급
+		
+		문제: 8개의 값을 갖는 배열을 생성, 모든 값을 나타내기
+		
+		풀이 - int[] marks = {1,2,3,4,5,6,7,8}
+			  marks.length
+			  for(int i = 0; i < marks.length; i++){
+			  	System.out.plintln(marks[i]);
+			  }
+		
 */		
 	}
 }
