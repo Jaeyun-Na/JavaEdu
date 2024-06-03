6ade9ba4c8668a4aafff52f8de29006a80d878e8
Jaeyun <Jaeyun@DESKTOP-TK0JOHI>
2024-06-03 15:14:31 +0900

    240603

--- kr.co.green.homeEdu/src/com/in28minutes/arrays/Student.java
+++ kr.co.green.homeEdu/src/com/in28minutes/arrays/Student.java
@@ -1,5 +1,8 @@
 package com.in28minutes.arrays;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Student {
 
 	private String name;
@@ -14,5 +17,49 @@ public class Student {
 		// TODO Auto-generated method stub
 		return marks.length;
 	}
+	public int getTotalSumOfMarks() {
+		int sum = 0;
+		for(int mark : marks) {
+			sum += mark;
+		}
+		return sum;
+	}
 
+	public int getMaximumMark() {
+//		int maximum = 0;
+		int maximum = Integer.MIN_VALUE;;
+		for(int mark : marks) {
+			if(mark > maximum) {
+				maximum = mark;
+			}
+		}
+		return maximum;
+	}
+
+	public int getMinimumMark() {
+		int minimum = Integer.MAX_VALUE;
+		for(int mark : marks) {
+			if(mark < minimum) {
+				minimum = mark;
+			}
+		}
+		return minimum;
+	}
+
+	public BigDecimal getAverageMarks() {
+		//sum
+		int sum = getTotalSumOfMarks();
+		int number = getNumberOfMarks();
+		//total
+		
+//		정수만 나옴
+//		return new BigDecimal(sum/number);
+//		소수까지 포함
+		return new BigDecimal(sum).divide(new BigDecimal(number),3, RoundingMode.UP);
+//		98.66666666666 precision (3) 98.667 
+//		소수점 3자리만 표시 그및은 반올림 처리
+//		98.6665 Precision(3) Rounding mode(up/down)
+//		소수점 3자리 표시 4번째 자리 올림/배림
+		
+	}
 }
--- kr.co.green.homeEdu/src/com/in28minutes/arrays/studentRunner.java
+++ kr.co.green.homeEdu/src/com/in28minutes/arrays/studentRunner.java
@@ -1,5 +1,7 @@
 package com.in28minutes.arrays;
 
+import java.math.BigDecimal;
+
 public class studentRunner {
 /*
 	Student student = new Student(name. list of marks);
@@ -11,10 +13,24 @@ public class studentRunner {
  */
 
 	public static void main(String[] args) {
-		int [] marks = {99,98,100};
+		int [] marks = {97,98,100};
+		
 		Student student = new Student("Kim",marks);
+		
 		int number = student.getNumberOfMarks();
 		System.out.println("number of marks : " + number);
+		
+		int sum = student.getTotalSumOfMarks();
+		System.out.println("sum of marks : " + sum);
+		
+		int maximumMark = student.getMaximumMark();
+		System.out.println("maximum of marks : " + maximumMark);
+		
+		int minimumMark = student.getMinimumMark();
+		System.out.println("minimum of marks : " + minimumMark);
+		
+		BigDecimal average = student.getAverageMarks();
+		System.out.println("average is : " + average);
 	}
 
 }
