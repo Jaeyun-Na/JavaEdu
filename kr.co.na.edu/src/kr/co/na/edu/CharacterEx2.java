2fdc24b4134b9e90ef523eebf9833e3377b8bd37
jaeyun <gejon30@naver.com>
2024-12-16 12:20:57 +0900

    .

--- kr.co.na.edu/src/kr/co/na/edu/CharacterEx2.java
+++ kr.co.na.edu/src/kr/co/na/edu/CharacterEx2.java
@@ -17,7 +17,7 @@ public class CharacterEx2 {
 		int b = -66;
 		
 		char a2 = 65;
-		//char b2 = -66;	//문자형 변수에 음수를 넣으면 오류 발생(확인 후 주석처리함)
+		//char b2 = -66;	//문자형 변수에 음수를 넣으면 오류 발생(확인 후 주석처리함) 
 		
 		System.out.println((char)a);
 		System.out.println((char)b);
