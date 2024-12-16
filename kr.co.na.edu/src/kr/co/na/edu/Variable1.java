e9f1d0787b9a0143488e49db1b39052fb00ec696
Ohlle.Hoon <gejon30@naver.com>
2024-12-16 11:48:15 +0900

    24.12.16 자료형

--- kr.co.na.edu/src/kr/co/na/edu/Variable1.java
+++ kr.co.na.edu/src/kr/co/na/edu/Variable1.java
@@ -5,8 +5,11 @@ public class Variable1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+/*
 		int level; //정수형 변수 level 선언
 		level = 10; //level 변수값 대입
+*/
+		int level = 10; // 변수의 생성과 동시에 값을 대입(초기화)
 		System.out.println(level); //level값을 출력
 	}
 
