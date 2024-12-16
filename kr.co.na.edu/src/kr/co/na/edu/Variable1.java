9d3d272f95329e1c48c0fff6c3d52dd5bbd88e23
Ohlle.Hoon <gejon30@naver.com>
2024-12-16 11:02:30 +0900

    새로시작하는 공부

---
+++ kr.co.na.edu/src/kr/co/na/edu/Variable1.java
@@ -0,0 +1,13 @@
+package kr.co.na.edu;
+
+public class Variable1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int level; //정수형 변수 level 선언
+		level = 10; //level 변수값 대입
+		System.out.println(level); //level값을 출력
+	}
+
+}
