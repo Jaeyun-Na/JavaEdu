f2d6b8d10bfac37bd1090d8f1da77321fd4019ae
jaeyun <gejon30@naver.com>
2024-12-16 12:55:18 +0900

    24.12.16 자료형

---
+++ kr.co.na.edu/src/kr/co/na/edu/BooleanEx.java
@@ -0,0 +1,13 @@
+package kr.co.na.edu;
+
+public class BooleanEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		boolean isMarried = false;	//boolean변수를 초기화. boolean변수는 true나 false만 대인할수 있으며, 결과값도 true,false로만 출려됨. 다른 문자나,숫자를 넣으면 오류 발생
+		System.out.println(isMarried);
+		
+	}
+
+}
