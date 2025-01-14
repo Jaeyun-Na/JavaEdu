ac3c7bb9233143a5eb5dae438b08b85077859ad3
gejon <gejon@DESKTOP-1ILBTPR>
2025-01-14 12:25:14 +0900

    25.01.14

--- kr.co.na.edu/src/kr/co/na/edu/BooleanEx.java
+++ kr.co.na.edu/src/kr/co/na/edu/BooleanEx.java
@@ -8,6 +8,12 @@ public class BooleanEx {
 		boolean isMarried = false;	//boolean변수를 초기화. boolean변수는 true나 false만 대인할수 있으며, 결과값도 true,false로만 출려됨. 다른 문자나,숫자를 넣으면 오류 발생
 		System.out.println(isMarried);
 		
+		byte bNum = 10;
+	    int iNum = bNum;  //byte형 값이 int형 변수로 대입됨
+
+	    System.out.println(bNum);
+	    System.out.println(iNum);
+		
 	}
 
 }
